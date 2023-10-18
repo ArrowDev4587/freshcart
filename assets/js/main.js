fcc5cb03ca90a90449c39cfcfc5338a1ff8431e4
Ahmet Dursun <82242698+ArrowDev4587@users.noreply.github.com>
2023-10-18 22:11:55 +0300

    Featured Slider Added

---
+++ assets/js/main.js
@@ -0,0 +1,21 @@
+function langDropdown() {
+    var lang_div = document.getElementsByClassName("lang-dropdown-content")[0];
+    if (lang_div.style.display === "none") {
+        lang_div.style.display = "block";
+    } else {
+        lang_div.style.display = "none";
+    }
+}
+
+const s1 = new Swiper('.s1', {
+    direction: 'horizontal',
+    loop: false,
+});
+
+const s2 = new Swiper('.s2', {
+    direction: 'horizontal',
+    loop: false,
+    slidesPerView: 6,
+    freeMode: true,
+    spaceBetween: 20,
+});
