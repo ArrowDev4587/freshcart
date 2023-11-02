03df5511668370cfdd157a5b9d9f1442cfb8e680
Ahmet Dursun <82242698+ArrowDev4587@users.noreply.github.com>
2023-11-02 22:20:23 +0300

    Fixed some bugs.

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,6 +7,15 @@ function langDropdown() {
     }
 }
 
+function accountDropdown() {
+    var account_div = document.getElementsByClassName("account-dropdown-content")[0];
+    if (account_div.style.display === "none") {
+        account_div.style.display = "flex";
+    } else {
+        account_div.style.display = "none";
+    }
+}
+
 const s1 = new Swiper('.s1', {
     direction: 'horizontal',
     loop: false,
