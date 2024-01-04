a8bc3f7f0e8fb017a9d64c3f57a6c1bdf245aca8
Ahmet Dursun <ahmetdursun4587@protonmail.com>
2024-01-04 23:46:33 +0300

    Created product detail page, added test slider for later.

--- assets/js/main.js
+++ assets/js/main.js
@@ -83,3 +83,24 @@ document.addEventListener("DOMContentLoaded", function() {
         });
     });
 });
+
+
+var swiper = new Swiper(".mySwiper", {
+    loop: true,
+    spaceBetween: 10,
+    slidesPerView: 4,
+    freeMode: true,
+    watchSlidesProgress: true,
+  });
+  var swiper2 = new Swiper(".mySwiper2", {
+    loop: false,
+    spaceBetween: 10,
+    freeMode: false,
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    thumbs: {
+      swiper: swiper,
+    },
+  });
